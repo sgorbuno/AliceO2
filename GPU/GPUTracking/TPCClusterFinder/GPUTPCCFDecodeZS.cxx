a6f46958043a6ba4aa9b177cf49d6bde72511052
Sergey Gorbunov <gorbunov@dev04.fias.uni-frankfurt.de>
2020-02-25 21:14:02 +0100

    update

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCCFDecodeZS.cxx
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCCFDecodeZS.cxx
@@ -42,46 +42,49 @@ GPUdii() void GPUTPCCFDecodeZS::Thread<GPUTPCCFDecodeZS::decodeZS>(int nBlocks, 
 
 GPUd() void GPUTPCCFDecodeZS::decode(GPUTPCClusterFinder& clusterer, GPUSharedMemory& s, int nBlocks, int nThreads, int iBlock, int iThread)
 {
+  
   const unsigned int slice = clusterer.mISlice;
   const unsigned int endpoint = iBlock;
-  const GPUTrackingInOutZS::GPUTrackingInOutZSSlice& zs = clusterer.GetConstantMem()->ioPtrs.tpcZS->slice[slice];
-
-  //unsigned int s_RowClusterOffset[o2::tpc::TPCZSHDR::TPC_MAX_ZS_ROW_IN_ENDPOINT];
+  const GPUTrackingInOutZS::GPUTrackingInOutZSSlice& zs = clusterer.GetConstantMem()->ioPtrs.tpcZS->slice[slice];  
 
   if (zs.count[endpoint] == 0) {
-    return;
+    // return;
   }
   deprecated::PackedDigit* digits = clusterer.mPdigits;
-  const size_t nDigits = clusterer.mPmemory->nDigitsOffset[endpoint];
-  const unsigned int* pageSrc = (const unsigned int*)(((const unsigned char*)zs.zsPtr[endpoint][0]));
+  //const size_t nDigits = clusterer.mPmemory->nDigitsOffset[endpoint];
+  /*
+  const unsigned int* pageSrc = (const unsigned int*)(((const unsigned char*)zs.zsPtr[endpoint][0]));   
 
-  GPUbarrier();
-  CA_SHARED_CACHE_REF(&s.ZSPage[0], pageSrc, TPCZSHDR::TPC_ZS_PAGE_SIZE, unsigned int, pageCache);
-  GPUbarrier();
-
-  const unsigned char* page = (const unsigned char*)pageCache;
-
-  const unsigned char* pagePtr = page + sizeof(o2::header::RAWDataHeader) + sizeof(TPCZSHDR);
+   unsigned char* page = ( unsigned char*)pageSrc;
+   unsigned char* pagePtr = page + sizeof(o2::header::RAWDataHeader) + sizeof(TPCZSHDR);
   pagePtr += (pagePtr - page) & 1; //Ensure 16 bit alignment
-
-  const TPCZSTBHDR* tbHdr = reinterpret_cast<const TPCZSTBHDR*>(pagePtr);
-
-  const int nRowsUsed = CAMath::Popcount(tbHdr->rowMask & 0x7FFF);
-  const unsigned short* arr = tbHdr->rowAddr1();
+   TPCZSTBHDR* tbHdr = reinterpret_cast< TPCZSTBHDR*>(pagePtr);
+  //const int nRowsUsed = CAMath::Popcount(tbHdr->rowMask & 0x7FFF);
+*/
+GPUshared()  unsigned char pp[1000];
+unsigned char* arr = reinterpret_cast< unsigned char*> (pp);
 
   if (iThread != 0)
     return;
+ 
+
+for( int i=0; i<9; i++) arr[i] = 10*i;
+pp[0]=9;
+
+GPUbarrier();
+
+const int nRowsUsed = pp[0];
 
   unsigned int tmpOutput = 0;
 
   for (int iter = 0; iter < 1000000; iter++) {
     for (int n = 1; n < nRowsUsed; n++) {
-      const unsigned char* rowData = (page + arr[n - 1]);
+      const unsigned char* rowData = (pp + arr[n - 1]);
       tmpOutput += rowData[2 * *rowData];
     }
   }
 
   if (iThread == 0) {
-    digits[nDigits].time = tmpOutput;
+    digits[0].time = tmpOutput;
   }
 }
