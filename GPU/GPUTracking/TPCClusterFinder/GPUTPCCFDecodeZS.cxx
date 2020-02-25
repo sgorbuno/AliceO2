311a9613a146b095b71cfeb976a1e6c86b16aa10
root <root@dev04.fias.uni-frankfurt.de>
2020-02-25 08:17:49 +0000

    update

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCCFDecodeZS.cxx
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCCFDecodeZS.cxx
@@ -55,8 +55,6 @@ GPUd() void GPUTPCCFDecodeZS::decode(GPUTPCClusterFinder& clusterer, GPUSharedMe
   const size_t nDigits = clusterer.mPmemory->nDigitsOffset[endpoint];
   unsigned int rowOffsetCounter = 0;
 
-  const unsigned int s_decodeBits = TPCZSHDR::TPC_ZS_NBITS_V1;
-
   unsigned int tmpOutput = 0;
 
   const unsigned int* pageSrc = (const unsigned int*)(((const unsigned char*)zs.zsPtr[endpoint][0]));
@@ -65,52 +63,43 @@ GPUd() void GPUTPCCFDecodeZS::decode(GPUTPCClusterFinder& clusterer, GPUSharedMe
   CA_SHARED_CACHE_REF(&s.ZSPage[0], pageSrc, TPCZSHDR::TPC_ZS_PAGE_SIZE, unsigned int, pageCache);
   GPUbarrier();
 
+  const unsigned char* __restrict__ page = (const unsigned char*)pageCache;
+
+  const unsigned char* pagePtr = page + sizeof(o2::header::RAWDataHeader);
+  const TPCZSHDR* hdr = reinterpret_cast<const TPCZSHDR*>(pagePtr);
+  pagePtr += sizeof(*hdr);
+  pagePtr += (pagePtr - page) & 1; //Ensure 16 bit alignment
+  const TPCZSTBHDR* tbHdr = reinterpret_cast<const TPCZSTBHDR*>(pagePtr);
+  const unsigned int mask = ((const unsigned int)tbHdr->rowMask) & 0x7FFF;
+  if (mask == 0) {
+    return;
+  }
+  const int nRowsUsed = CAMath::Popcount(mask);
+  pagePtr += 2 * nRowsUsed;
+  const auto sg1 = tbHdr->rowAddr1();
+
   if (iThread != 0)
     return;
 
   for (int iter = 0; iter < 100000; iter++) {
-    const unsigned char* __restrict__ page = (const unsigned char*)pageCache;
-    const unsigned char* pagePtr = page + sizeof(o2::header::RAWDataHeader);
-    const TPCZSHDR* hdr = reinterpret_cast<const TPCZSHDR*>(pagePtr);
-    pagePtr += sizeof(*hdr);
-    const int nTimeBins = hdr->nTimeBins;
-    //for (int l = 0; l < nTimeBins; l++) {
-         for (int l = 0; l < 1; l++) {
- 
-      pagePtr += (pagePtr - page) & 1; //Ensure 16 bit alignment
-      const TPCZSTBHDR* tbHdr = reinterpret_cast<const TPCZSTBHDR*>(pagePtr);
-      const unsigned int mask = ((const unsigned int)tbHdr->rowMask) & 0x7FFF;
-      if (mask == 0) {
-        pagePtr += 2;
-        continue;
+
+    //GPUbarrier();
+    { //if (iThread == 0) {
+      {
+        //s_RowClusterOffset[0] = rowOffsetCounter;
+        const unsigned char* __restrict__ rowData = pagePtr;
+        unsigned int tmp = *rowData;
+        rowOffsetCounter += rowData[tmp << 1]; // Sum up number of ADC samples per row to compute offset in target buffer
       }
-      const int nRowsUsed = CAMath::Popcount(mask);
-      pagePtr += 2 * nRowsUsed;
-      const auto sg1 = tbHdr->rowAddr1();
-      //GPUbarrier();
-      { //if (iThread == 0) {
-        {
-          //s_RowClusterOffset[0] = rowOffsetCounter;
-          const unsigned char* __restrict__ rowData = pagePtr;
-          unsigned int tmp = *rowData;
-          rowOffsetCounter += rowData[tmp << 1]; // Sum up number of ADC samples per row to compute offset in target buffer
-        }
-        for (int n = 1; n < nRowsUsed; n++) {
-          //s_RowClusterOffset[n] = rowOffsetCounter;
-          const unsigned char* __restrict__ rowData = (page + sg1[n - 1]);
-          unsigned int tmp = *rowData;
-          rowOffsetCounter += rowData[tmp << 1]; // Sum up number of ADC samples per row to compute offset in target buffer
-        }
+      for (int n = 1; n < nRowsUsed; n++) {
+        //s_RowClusterOffset[n] = rowOffsetCounter;
+        const unsigned char* __restrict__ rowData = (page + sg1[n - 1]);
+        unsigned int tmp = *rowData;
+        rowOffsetCounter += rowData[tmp << 1]; // Sum up number of ADC samples per row to compute offset in target buffer
       }
-      //GPUbarrier();
-      tmpOutput += rowOffsetCounter;
-      if (nRowsUsed > 1) {
-        //pagePtr = page + tbHdr->rowAddr1()[nRowsUsed - 2];
-        pagePtr = page + sg1[nRowsUsed - 2];
-      }
-      pagePtr += 2 * *pagePtr;                          // Go to entry for last sequence length
-      pagePtr += 1 + (*pagePtr * s_decodeBits + 7) / 8; // Go to beginning of next time bin
     }
+    //GPUbarrier();
+    tmpOutput += rowOffsetCounter;
   }
 
   if (iThread == 0) {
