d6576746d18cb4b767328d7d0025bf41880a714b
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2020-02-28 02:13:12 +0100

    update

--- GPU/GPUTracking/SliceTracker/GPUTPCNeighboursFinder.cxx
+++ GPU/GPUTracking/SliceTracker/GPUTPCNeighboursFinder.cxx
@@ -69,7 +69,7 @@ GPUdii() void GPUTPCNeighboursFinder::Thread<0>(int /*nBlocks*/, int nThreads, i
 #endif
 
   int linkDn = -1;
-
+ int bestD = 100000;
   const float kAreaSize = tracker.mConstantMem->param.rec.NeighboursSearchArea;
 
   for (int ih = iThread; ih < row.mNHits; ih += nThreads) {
@@ -85,18 +85,16 @@ GPUdii() void GPUTPCNeighboursFinder::Thread<0>(int /*nBlocks*/, int nThreads, i
     reinterpret_cast<const GPUTPCRow&>(row).Grid().GetBin(y + kAreaSize, z + kAreaSize, &binYmax, &binZmax);
     nY = reinterpret_cast<const GPUTPCRow&>(row).Grid().Ny();
 
-    int bestD = 100000;
-
     for (int k1 = binZmin; k1 <= binZmax; k1++) {
       int iMin = lFirstHitInBin[lFirstHitInBinOffset + k1 * nY + binYmin];
       int iMax = lFirstHitInBin[lFirstHitInBinOffset + k1 * nY + binYmax + 1];
-      /*
+      
       int d = iMin + iMax;
       if (d < bestD) {
         bestD = d;
         linkDn = k1;
       }
-      */
+      /*
       for (int i = iMin; i < iMax; i++) {
         //HIPGPUglobalref() const cahit2& hitDataDn = pHitData[lHitNumberOffset + i];
         int d = i; //hitDataDn.x + hitDataDn.y;
@@ -105,6 +103,7 @@ GPUdii() void GPUTPCNeighboursFinder::Thread<0>(int /*nBlocks*/, int nThreads, i
           linkDn = i;
         }
       }
+      */
     }
   }
   tracker.mData.mLinkDownData[lHitNumberOffset + iThread] = linkDn;
