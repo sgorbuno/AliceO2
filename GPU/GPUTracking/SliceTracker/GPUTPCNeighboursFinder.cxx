bbd8a7734a24e834e31caed03b32abf478064267
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2020-03-04 04:31:57 +0100

    update

--- GPU/GPUTracking/SliceTracker/GPUTPCNeighboursFinder.cxx
+++ GPU/GPUTracking/SliceTracker/GPUTPCNeighboursFinder.cxx
@@ -1,124 +1,48 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
-//
-// See http://alice-o2.web.cern.ch/license for full licensing information.
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
 
-/// \file GPUTPCNeighboursFinder.cxx
-/// \author Sergey Gorbunov, Ivan Kisel, David Rohr
-
-#include "GPUTPCHit.h"
 #include "GPUTPCNeighboursFinder.h"
-#include "GPUTPCTracker.h"
-#include "GPUCommonMath.h"
 using namespace GPUCA_NAMESPACE::gpu;
-
-#if defined(__HIPCC__) && defined(GPUCA_GPUCODE_DEVICE)
-#define HIPGPUsharedref() __attribute__((address_space(3)))
-#define HIPGPUglobalref() __attribute__((address_space(1)))
-#define HIPGPUconstantref() __attribute__((address_space(4)))
-#else
-#define HIPGPUsharedref()
-#define HIPGPUglobalref()
-#define HIPGPUconstantref()
-#endif
 
 template <>
 GPUdii() void GPUTPCNeighboursFinder::Thread<0>(int /*nBlocks*/, int nThreads, int iBlock, int iThread, GPUsharedref() MEM_LOCAL(GPUSharedMemory) & GPUrestrict() ss, processorType& GPUrestrict() trackerX)
 {
-  //* find neighbours
-
-  int iRow = iBlock;
-
-  if ((iRow <= 1) || (iRow >= GPUCA_ROW_COUNT - 2)) {
-    return;
-  }
-
 #ifdef GPUCA_GPUCODE
-  for (unsigned int i = iThread; i < sizeof(MEM_PLAIN(GPUTPCRow)) / sizeof(int); i += nThreads) {
-    ((int*)&ss.mRow)[i] = ((int*)&trackerX.SliceDataRows()[iBlock])[i];
-  }
-  GPUbarrier();
-#endif
 
-  HIPGPUsharedref() MEM_LOCAL(GPUSharedMemory) & GPUrestrict() s = (HIPGPUsharedref() MEM_LOCAL(GPUSharedMemory)&)ss;
-  HIPGPUconstantref() processorType& GPUrestrict() tracker = (HIPGPUconstantref() processorType&)trackerX;
+#define doCompileCrash 1
 
-#ifdef GPUCA_GPUCODE
-  HIPGPUsharedref() const MEM_LOCAL(GPUTPCRow) & GPUrestrict() row = (HIPGPUsharedref() const MEM_LOCAL(GPUTPCRow)&)s.mRow;
+  /*  Problem 1:  --- Compilation crash --- 
+
+   The code does not not compile when doCompileCrash == 1
+   and compiles otherwise.
+  
+   Compilation error:
+
+   [ 98%] Built target ca
+    error: local memory limit exceeded (69916) in _Z27krnl_GPUTPCNeighboursFinderi
+    Generating AMD GCN kernel failed in llc for target: gfx906
+    Error: hc-kernel-assemble[166]: failed with status -1
+    clang-10: error: HC assembler command failed with exit code 255 (use -v to see invocation)
+    GPU/GPUTracking/Base/hip/CMakeFiles/GPUTrackingHIP.dir/build.make:62: recipe for target 'GPU/GPUTracking/Base/hip/CMakeFiles/GPUTrackingHIP.dir/GPUReconstructionHIP.hip.cxx.o' failed
+    make[2]: *** [GPU/GPUTracking/Base/hip/CMakeFiles/GPUTrackingHIP.dir/GPUReconstructionHIP.hip.cxx.o] Error 255
+    CMakeFiles/Makefile2:290: recipe for target 'GPU/GPUTracking/Base/hip/CMakeFiles/GPUTrackingHIP.dir/all' failed
+    make[1]: *** [GPU/GPUTracking/Base/hip/CMakeFiles/GPUTrackingHIP.dir/all] Error 2
+    Makefile:129: recipe for target 'all' failed
+    make: *** [all] Error 2
+
+  */
+
+  /*  Problem 2: 
+    ss.mRow.Grid().GetBin(..) is not optimised away, 
+    though Grid() and GetBin() are declared const and the output values 
+    binYmin, binZmin are not used anywhere.
+  */
+
+#if doCompileCrash == 1
+// do nothing
 #else
-  HIPGPUglobalref() const MEM_GLOBAL(GPUTPCRow) & GPUrestrict() row = (HIPGPUglobalref() const MEM_GLOBAL(GPUTPCRow)&)tracker.mData.mRows[iRow];
+  ss.mB[0][0] = 0; // write something to the shared memory
 #endif
 
-  const float y0 = row.mGrid.mYMin;
-  const float z0 = row.mGrid.mZMin;
-  const float stepY = row.mHstepY;
-  const float stepZ = row.mHstepZ;
-
-  const long int lHitNumberOffset = row.mHitNumberOffset;
-  const int lFirstHitInBinOffset = row.mFirstHitInBinOffset;
-  HIPGPUglobalref() const calink* GPUrestrict() lFirstHitInBin = (HIPGPUglobalref() const calink*)tracker.mData.mFirstHitInBin;
-  HIPGPUglobalref() const cahit2* GPUrestrict() pHitData = (HIPGPUglobalref() const cahit2*)tracker.mData.mHitData;
-
-#if GPUCA_NEIGHBOURS_FINDER_MAX_NNEIGHUP > 0
-  s.mB[0][iThread] = (calink)0;
+  int binYmin, binZmin;
+  reinterpret_cast<const GPUTPCRow&>(ss.mRow).Grid().GetBin(0.f, 0.f, &binYmin, &binZmin);
 #endif
-
-  int linkDn = -1;
-  float bestD = 100000;
-  const float kAreaSize = tracker.mConstantMem->param.rec.NeighboursSearchArea;
-
-  for (int ih = iThread; ih < row.mNHits; ih += nThreads) {
-
-    HIPGPUglobalref() const cahit2& hitData = pHitData[lHitNumberOffset + ih];
-    const float y = y0 + (hitData.x) * stepY;
-    const float z = z0 + (hitData.y) * stepZ;
-
-    int binYmin, binYmax, binZmin, binZmax;
-    int nY;
-
-    reinterpret_cast<const GPUTPCRow&>(row).Grid().GetBin(y - kAreaSize, z - kAreaSize, &binYmin, &binZmin);
-    reinterpret_cast<const GPUTPCRow&>(row).Grid().GetBin(y + kAreaSize, z + kAreaSize, &binYmax, &binZmax);
-    nY = reinterpret_cast<const GPUTPCRow&>(row).Grid().Ny();
-    int nZ = reinterpret_cast<const GPUTPCRow&>(row).Grid().Nz();
-    //continue;
-    int d = 0;
-    // #pragma unroll(1)
-    for (int k1 = binZmin; k1 <= binZmax; k1++) {
-      //#pragma unroll(16)
-      // for (int k1 = 0; k1 <= nZ; k1++) {
-      int iMin = lFirstHitInBin[lFirstHitInBinOffset + k1 * nY + binYmin];
-      int iMax = lFirstHitInBin[lFirstHitInBinOffset + k1 * nY + binYmax + 1];
-      d = iMin + iMax;
-      if (d < bestD) {
-        bestD = d;
-        linkDn = k1;
-      }
-
-      //HIPGPUglobalref() const cahit2 &hitDataDn = pHitData[0];
-      // #pragma unroll(2)      
-      for (int i = iMin-1; ++i < iMax; ) {
-      //int i = iMin;
-      //while (i < iMax) {
-        HIPGPUglobalref() const cahit2& hitDataDn = pHitData[lHitNumberOffset + i];
-        //d = d + i;
-        //i++;
-        d = hitDataDn.x + hitDataDn.y;
-        if (d < 1) {
-          //i++;
-          continue;
-        }
-        if (d < bestD) {
-          bestD = d;
-          linkDn = i;
-        }        
-        //i++;
-      }
-    }
-  }
-  tracker.mData.mLinkDownData[lHitNumberOffset + iThread] = linkDn;
 }
